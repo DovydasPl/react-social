6e4eca0d72771e8f7504aadf0d9fb34a2a3bd0d9
DovydasPl <dovydaspladas@gmail.com>
2023-11-12 22:05:09 +0200

    a

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAsuNx9VHjiRw5Kcg5clO4gaPC9asksTT8",
-    authDomain: "react-social-952b9.firebaseapp.com",
-    projectId: "react-social-952b9",
-    storageBucket: "react-social-952b9.appspot.com",
-    messagingSenderId: "535796169959",
-    appId: "1:535796169959:web:c85b3229b6b37d49c5f642",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
 
 // Initialize Firebase
